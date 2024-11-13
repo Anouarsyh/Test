dd311e3aac5c88988b935946ef0e874bac3a6e1e
Anouar Sayah <anouar.sayah03@gmail.com>
2024-11-13 20:54:56 +0100

    Removed MongoDB and updated app code

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const lodash = require('lodash');
 const debug = require('debug')('app:server');
 
@@ -12,11 +11,6 @@ app.get('/', (req, res) => {
     debug(message); // Using debug to log to console
     res.send(message);
 });
-
-// Simulating a database connection with mongoose
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('MongoDB connection error:', err));
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
